bd1636a26d27ee1d4bd9505af5dd3accad0fb619
AlfredoMartinezCantero <alfredomartinezcantero@gmail.com>
2026-01-29 12:07:47 +0100

    Add files via upload

---
+++ alfredocarrusel.js
@@ -0,0 +1,34 @@
+let contenedor = document.querySelector(".alfredocarrusel");
+        let contenido = document.querySelectorAll(".alfredocarrusel img")
+
+        contenido.forEach(function(elemento){
+            elemento.remove()
+        })
+
+        let nuevo_contenedor = document.createElement("section")
+        contenedor.appendChild(nuevo_contenedor)
+
+        contenido.forEach(function(elemento){
+            nuevo_contenedor.appendChild(elemento)
+        })
+
+        let botonatras = document.createElement("button")
+        botonatras.textContent = "◀"
+        contenedor.appendChild(botonatras)
+        
+        let botondelante = document.createElement("button")
+        botondelante.textContent = "▶"
+        contenedor.appendChild(botondelante)
+
+        var anchura = 1280;
+        var contador = 0;
+
+        botondelante.onclick = function(){
+            contador--;
+            nuevo_contenedor.style.left = contador * anchura + "px"
+        }
+      
+        botonatras.onclick = function(){
+            contador++;
+            nuevo_contenedor.style.left = contador * anchura + "px"
+        }
